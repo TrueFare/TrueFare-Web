5066be6cf8bd6eb8a3753b9cf9c34bb6ea8fcd49
JianZcar <esteban.jianzcar@outlook.com>
2026-02-19 14:40:16 +0800

    dynamically read tables

---
+++ server/api/[table].get.ts
@@ -0,0 +1,8 @@
+import { st } from "vue-router/dist/router-CWoNjPRp.mjs";
+
+export default defineEventHandler(async (event) => {
+  const table = event.context.params?.table as string;
+  const db = event.context.cloudflare.env.truefare_db;
+  const result = await db.prepare(`SELECT * FROM ${table}`).run();
+  return result;
+});
--- server/api/users.get.ts
+++
@@ -1,5 +0,0 @@
-export default defineEventHandler(async (event) => {
-  const db = event.context.cloudflare.env.truefare_db;
-  const result = await db.prepare("SELECT * FROM USER").run();
-  return result;
-});
