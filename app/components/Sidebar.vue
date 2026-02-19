b23ecb05a211f881c0a4e37675d331bda0aa4429
Carlos Heredero <herederocarlos9@gmail.com>
2026-02-19 12:59:22 +0800

    added toda

--- app/components/Sidebar.vue
+++ app/components/Sidebar.vue
@@ -1,9 +1,7 @@
 <template>
   <ul class="menu p-4 w-64 min-h-full bg-base-200 text-base-content">
     <li><NuxtLink to="/dashboard">Dashboard</NuxtLink></li>
-    <li><a>TODAs</a></li>
-    <li><a>Tricycles</a></li>
-    <li><a>Reports</a></li>
-    <li><a>Users</a></li>
+    <li><a>Profile</a></li>
+    <li><a>Logout</a></li>
   </ul>
 </template>
